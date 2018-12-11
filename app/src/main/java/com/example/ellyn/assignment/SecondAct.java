837e9a7d32bbbaabdf5613bcb19b3388fd63a4c7
Ellynnn <eilenanjl-wa16@student.tarc.edu.my>
2018-12-11 23:28:19 +0800

    Homepage

--- app/src/main/java/com/example/ellyn/assignment/SecondAct.java
+++ app/src/main/java/com/example/ellyn/assignment/SecondAct.java
@@ -1,7 +1,11 @@
 package com.example.ellyn.assignment;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SecondAct extends AppCompatActivity {
 
@@ -9,5 +13,42 @@ public class SecondAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        BottomNavigationView bottomNavi = findViewById(R.id.bottom_navigation);
+        bottomNavi.setOnNavigationItemSelectedListener(naviListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener naviListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch(item.getItemId()) {
+                case R.id.navi_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+
+                case R.id.navi_reminder:
+                    selectedFragment = new ReminderFragment();
+                    break;
+
+                case R.id.navi_donation:
+                    selectedFragment = new DonationFragment();
+                    break;
+
+                case R.id.navi_about_us:
+                    selectedFragment = new AboutUsFragment();
+                    break;
+
+                case R.id.navi_profile:
+                    selectedFragment = new ProfileFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
 }
