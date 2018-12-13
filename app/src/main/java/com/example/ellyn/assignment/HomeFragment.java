ce7b16d70d4c4608e66901b329ea9387ae3b10eb
Ellynnn <eilenanjl-wa16@student.tarc.edu.my>
2018-12-13 17:03:19 +0800

    Splash Screen

--- app/src/main/java/com/example/ellyn/assignment/HomeFragment.java
+++ app/src/main/java/com/example/ellyn/assignment/HomeFragment.java
@@ -6,11 +6,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AbsoluteLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ViewFlipper;
 
 public class HomeFragment extends Fragment {
+
+    View myView;
+    Animation fade_in, fade_out;
+    ViewFlipper viewFlipper;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        if(container==null) {
+            return inflater.inflate(R.layout.fragment_home, container, false);
+        }
+
+        myView = inflater.inflate(R.layout.fragment_home, container, false);
+        viewFlipper = myView.findViewById(R.id.slideshowViewFlipper);
+
+        fade_in = AnimationUtils.loadAnimation(getActivity(), android.R.anim.fade_in);
+        fade_out = AnimationUtils.loadAnimation(getActivity(), android.R.anim.fade_out);
+
+        viewFlipper.setInAnimation(fade_in);
+        viewFlipper.setOutAnimation(fade_out);
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.startFlipping();
+
+        return myView;
+
     }
 }
