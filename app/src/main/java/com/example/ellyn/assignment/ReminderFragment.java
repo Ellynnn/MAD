2e54bf27feabacc29b0e3309ba70f4e7f104baa6
Ellynnn <eilenanjl-wa16@student.tarc.edu.my>
2018-12-22 05:49:13 +0800

    reminder design halfway

--- app/src/main/java/com/example/ellyn/assignment/ReminderFragment.java
+++ app/src/main/java/com/example/ellyn/assignment/ReminderFragment.java
@@ -2,6 +2,7 @@ package com.example.ellyn.assignment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,17 @@ public class ReminderFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_reminder, container, false);
+        if(container==null) {
+            return inflater.inflate(R.layout.fragment_reminder, container, false);
+        }
+        View view =   inflater.inflate(R.layout.fragment_reminder,container,false);
+        FloatingActionButton reminderFloatingActionButton = view.findViewById(R.id.floatingButton);
+        reminderFloatingActionButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                getFragmentManager().beginTransaction().add(R.id.fragment_container, new Reminder2Fragment()).commit();
+            }
+        });
+        return view;
     }
 }
