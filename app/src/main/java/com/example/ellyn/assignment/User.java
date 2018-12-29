e4fef0b26ef79dd9c25b9ab420fca187929406e7
Ellynnn <eilenanjl-wa16@student.tarc.edu.my>
2018-12-30 00:44:31 +0800

    firebase

---
+++ app/src/main/java/com/example/ellyn/assignment/User.java
@@ -0,0 +1,22 @@
+package com.example.ellyn.assignment;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class User {
+    public String userID;
+    public String name;
+    public String username;
+    public String phone;
+    public String email;
+    public String password;
+
+    public User(String userID, String name, String username, String phone, String email, String password){
+        this.userID = userID;
+        this.name = name;
+        this.username = username;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+}
