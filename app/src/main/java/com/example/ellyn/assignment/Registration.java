4425dcbf13a7568ed1ab3604534f7fd436717da3
lszeying <szeying980903@gmail.com>
2018-12-30 16:58:45 +0800

    Login n Register

--- app/src/main/java/com/example/ellyn/assignment/Registration.java
+++ app/src/main/java/com/example/ellyn/assignment/Registration.java
@@ -1,13 +1,101 @@
 package com.example.ellyn.assignment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
 public class Registration extends AppCompatActivity {
+    private EditText inputrfullname, inputruserid, inputrphoneno , inputremail, inputrpassword;
+    private Button  btnregister, btnlinklogin;
+    private ProgressBar progressBar;
+    private FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-  }
+
+        //Get Firebase auth instance
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        btnregister = (Button) findViewById(R.id.btn_register);
+        inputrfullname = (EditText) findViewById(R.id.rEditname);
+        inputruserid = (EditText) findViewById(R.id.rEdituserid);
+        inputrphoneno = (EditText) findViewById(R.id.rEditphoneno);
+        inputremail = (EditText) findViewById(R.id.rEditEmail);
+        inputrpassword = (EditText) findViewById(R.id.rEditPassword);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        btnlinklogin = (Button) findViewById(R.id.btn_link_login);
+
+
+        btnlinklogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Registration.this, login.class);
+                startActivity(myIntent);
+            }
+        });
+
+        btnregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String fullname = inputrfullname.getText().toString().trim();
+                String userid = inputruserid.getText().toString().trim();
+                String phoneno = inputrphoneno.getText().toString().trim();
+                String email = inputremail.getText().toString().trim();
+                String password = inputrpassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(fullname)) {
+                    Toast.makeText(getApplicationContext(), "Enter full name!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(userid)) {
+                    Toast.makeText(getApplicationContext(), "Enter user ID!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(phoneno)) {
+                    Toast.makeText(getApplicationContext(), "Enter phoneno!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+
 }
--- app/src/main/java/com/example/ellyn/assignment/login.java
+++ app/src/main/java/com/example/ellyn/assignment/login.java
@@ -1,40 +1,107 @@
 package com.example.ellyn.assignment;
 
-
-import android.annotation.SuppressLint;
-import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
 public class login extends AppCompatActivity {
-    private Button btnLogin;
+    private EditText inputEmail, inputPassword;
+    private Button btnLogin,btnLinkRegister;
+    private FirebaseDatabase firebaseDatabase;
+    private ProgressBar progressBar;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Get the view from activity_login.xml
+        // set the view now
         setContentView(R.layout.activity_login);
-        // Locate the button in activity_login.xml
-        btnLogin = (Button) findViewById(R.id.btn_login);
 
-        // Capture login button clicks
-        btnLogin.setOnClickListener(new OnClickListener() {
+        //Get Firebase  instance
+        // firebaseDatabase = FirebaseDatabase.getInstance();
+
+        // if (firebaseDatabase.getCurrentUser() != null) {
+        // startActivity(new Intent(login.this, MainActivity.class));
+        //  finish();
+        //}
+
+
+
+        // Locate the button in activity_login.xml
+        inputEmail = (EditText) findViewById(R.id.EditEmail);
+        inputPassword = (EditText) findViewById(R.id.EditPassword);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLinkRegister = (Button) findViewById(R.id.btn_link_register);
+
+        // Capture register button click
+        btnLinkRegister.setOnClickListener(new OnClickListener() {
             public void onClick(View arg0) {
 
-                // Start SecondActivity.class
-                Intent myIntent = new Intent(login.this, SecondActivity.class);
+                // Start login.class
+                Intent myIntent = new Intent(login.this, Registration.class);
                 startActivity(myIntent);
             }
         });
 
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                //authenticate user
+                //firebaseDatabase.signInWithEmailAndPassword(email, password)
+                //   .addOnCompleteListener(login.this, new OnCompleteListener<DatabaseReference>() {
+                //    @Override
+                //  public void onComplete(@NonNull Task<DatabaseReference> task) {
+                // If sign in fails, display a message to the user. If sign in succeeds
+                // the auth state listener will be notified and logic to handle the
+                // signed in user can be handled in the listener.
+                // progressBar.setVisibility(View.GONE);
+                //if (!task.isSuccessful()) {
+                // there was an error
+                //  if (password.length() < 6) {
+                //    inputPassword.setError(getString(R.string.minimum_password));
+                //} else {
+                //    Toast.makeText(login.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                //}
+                //} else {
+                //  Intent intent = new Intent(login .this, MainActivity.class);
+                //startActivity(intent);
+                //finish();
+                // }
+                //}
+                //});
+            }
+        });
     }
 }
-
-
